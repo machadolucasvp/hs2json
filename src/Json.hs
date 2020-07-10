21a662a7e340dff5a6faf7915c408efba0698b29
Lucas Machado <machadolucasvp@gmail.com>
2020-07-09 22:24:28 -0300

    feat: add Json data type definition

---
+++ src/Json.hs
@@ -0,0 +1,38 @@
+module Json where
+
+data Json = JsonString String
+                | JsonNumber Double
+                | JsonBool Bool
+                | JsonNull
+                | JsonObject[(String, Json)]
+                | JsonArray [Json]
+                    deriving (Eq, Ord, Show)
+
+getString :: Json -> Maybe String
+getString (JsonString s) = Just s
+getString _              = Nothing
+
+getInt :: Json -> Maybe Int
+getInt (JsonNumber n) = Just (truncate n)
+getInt _           = Nothing
+
+getDouble :: Json -> Maybe Double
+getDouble (JsonNumber n) = Just n
+getDouble _           = Nothing
+
+getBool :: Json -> Maybe Bool
+getBool (JsonBool b) = Just b
+getBool _         = Nothing
+
+getObject :: Json -> Maybe [(String, Json)]
+getObject (JsonObject o) = Just o
+getObject _           = Nothing
+
+getArray :: Json -> Maybe [Json]
+getArray (JsonArray a) = Just a
+getArray _          = Nothing
+
+isNull :: Json -> Bool
+isNull v = v == JsonNull
+
+
